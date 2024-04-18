2dbb01baaa018e85d4ef38cc08f041a32edde99c
natalia.belzarova <natalia.belzarova@gmail.com>
2024-04-18 14:22:39 +0200

    create CreatePoll and Polls livewire components

--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Poll extends Model
 {
+    protected $fillable = ['title'];
+
     use HasFactory;
 
     public function options()
---
+++ resources/views/livewire/create-poll.blade.php
@@ -0,0 +1,35 @@
+<div>
+  <form wire:submit.prevent="createPoll">
+    <label>Poll Title</label>
+
+    <input type="text" wire:model.live="title" />
+
+    @error('title')
+      <div class="text-red-500">{{$message}}</div>
+    @enderror
+
+
+    <div class="my-4">
+        <button class="btn" wire:click.prevent="addOption">Add Option</button>
+
+    </div>
+
+      @foreach ($options as $index=>$option)
+          <div class="mb-4">
+            <label for="">Option {{ $index +1 }}</label>
+          
+            <div class="flex gap-2">
+              <input type="text" wire:model.live="options.{{ $index }}">
+              <button class="btn" wire:click.prevent="removeOption({{$index}})">Remove</button>
+            </div>
+            @error("options.{$index}")
+              <div class="text-red-500">{{$message}}</div>
+            @enderror
+          </div>
+
+      @endforeach
+    <button type="submit" class="btn">Create Poll</button>
+
+
+  </form>
+</div>
